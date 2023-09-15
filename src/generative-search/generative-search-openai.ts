6d6332ca8b65fa93f23fa51464df73a68a0b424a
Duda Nogueira <dudanogueira@gmail.com>
2023-09-15 09:44:31 -0300

    created lab command, to be a "base function"

--- src/generative-search/generative-search-openai.ts
+++ src/generative-search/generative-search-openai.ts
@@ -1,76 +1,95 @@
-import weaviate, { WeaviateClient } from 'weaviate-ts-client';
+import weaviate from 'weaviate-ts-client';
 import axios from 'axios';
 
-const client = weaviate.client({
-  scheme: 'http',
-  host: 'localhost:8080',
-});
-// Delete the schema. CAUTION: THIS WILL DELETE YOUR DATA
-client.schema.classDeleter().withClassName("JeopardyQuestion").do()
+async function Do() {
 
-// Define our schema
-const schema = {
-  "class": "JeopardyQuestion",
-  "description": "List of jeopardy questions",
-  "vectorizer": "text2vec-openai",
-  "moduleConfig": {
-    "generative-openai": {
-      "model": "gpt-3.5-turbo",  // Optional - Defaults to `gpt-3.5-turbo`
-    }
-  },
-  "properties": [
-    {
-      "name": "Category",
-      "dataType": ["text"],
-      "description": "Category of the question",
+  // lets instantiate Weaviate client
+  const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+    //
+    // You can specify OPENAI-API-KEY both here or in the environment variable
+    // in our case, we already defined it in docker-compose.yml, so no need here
+    //
+    //   headers: { 'X-OpenAI-Api-Key': 'YOUR-OPENAI-API-KEY' },
+  });
+
+  //
+  // if you are running in WCS (Weaviate Cloud Service), you can use as bellow:
+  //
+  // const client: WeaviateClient = weaviate.client({
+  //   scheme: 'https',
+  //   host: 'some-endpoint.weaviate.network',  // Replace with your endpoint
+  //   apiKey: new ApiKey('YOUR-WEAVIATE-API-KEY'),  // Replace w/ your Weaviate instance API key
+  //   headers: { 'X-OpenAI-Api-Key': 'YOUR-OPENAI-API-KEY' },  // Replace with your inference API key
+  // });
+
+  // First, let's check if our class exists, so we can remove it and create a brand new
+  try {
+    let class_exists = await client.schema.classGetter().withClassName("JeopardyQuestion").do()
+    console.log("class found!")
+    // lets delete it
+    let delete_class = await client.schema.classDeleter().withClassName("JeopardyQuestion").do()
+    console.log("class deleted")
+  } catch (e) {
+    console.log("No class found.")
+  }
+  // this is our schema definition
+  // Define our schema
+  const schema_definition = {
+    "class": "JeopardyQuestion",
+    "description": "List of jeopardy questions",
+    "vectorizer": "text2vec-openai",
+    "moduleConfig": {
+      "generative-openai": {
+        "model": "gpt-3.5-turbo",  // Optional - Defaults to `gpt-3.5-turbo`
+      }
     },
-    {
-      "name": "Question",
-      "dataType": ["text"],
-      "description": "The question",
-    },
-    {
-      "name": "Answer",
-      "dataType": ["text"],
-      "description": "The answer",
-    }
-  ]
-}
+    "properties": [
+      {
+        "name": "Category",
+        "dataType": ["text"],
+        "description": "Category of the question",
+      },
+      {
+        "name": "Question",
+        "dataType": ["text"],
+        "description": "The question",
+      },
+      {
+        "name": "Answer",
+        "dataType": ["text"],
+        "description": "The answer",
+      }
+    ]
+  }
+  // lets create it
+  let new_class = await client.schema.classCreator().withClass(schema_definition).do()
+  console.log("We have a new class!", new_class["class"])
 
-// Lets create and log it
-client.schema.classCreator().withClass(schema).do().then(new_class => {
-  console.log(new_class)
-})
-
-// Grab our Jeopardy Questions
-const url = "https://raw.githubusercontent.com/weaviate/weaviate-examples/main/jeopardy_small_dataset/jeopardy_tiny.json"
-
-async function axiosGetData() {
-  const response = await axios.get(url)
-  return response.data
-}
-
-axios.get(url).then(response => {
-  console.log(`Data sample: ${JSON.stringify(response.data[0])}`)
-  // OUTPUT: Data sample: {"Category":"SCIENCE","Question":"This organ removes excess glucose from the blood & stores it as glycogen","Answer":"Liver"}
+  // now is time to import some data
+  // first, lets grab our Jeopardy Questions from the interwebs
+  const url = "https://raw.githubusercontent.com/weaviate/weaviate-examples/main/jeopardy_small_dataset/jeopardy_tiny.json"
+  const jeopardy_questions = await axios.get(url)
+  // this is our data sample
+  console.log(`Data sample: ${JSON.stringify(jeopardy_questions.data[0])}`)
+  // {"Category":"SCIENCE","Question":"This organ removes excess glucose from the blood & stores it as glycogen","Answer":"Liver"}
   let batcher = client.batch.objectsBatcher();
-  for (const dataObj of response.data) {
+  for (const dataObj of jeopardy_questions.data) {
     batcher = batcher.withObject({
       class: "JeopardyQuestion",
       properties: dataObj,
       // tenant: 'tenantA'  // If multi-tenancy is enabled, specify the tenant to which the object will be added.
     });
+    batcher.do();
   }
 
-  // Flush
-  batcher.do().then(console.log)
-
-  // now we can generate something:
-  const prompt = "Turn the following Jeogrady question into a Facebook Ad: {question}"
-  client.graphql
+  // now we can generate something from each of the results:
+  const prompt = "Turn the following Jeopardy question into a Facebook Ad: {question}."
+  const singlePrompt = await client.graphql
     .get()
     .withClassName('JeopardyQuestion')
-    .withFields('question')
+    .withFields('question category')
     .withNearText({
       concepts: ['Elephants'],
     })
@@ -78,7 +97,10 @@ axios.get(url).then(response => {
       singlePrompt: prompt,
     })
     .withLimit(1)
-    .do().then(response => {
-      console.log(JSON.stringify(response, null, 2));
-    });
-})
+    .do();  
+  console.log("Single Prompt response:", JSON.stringify(singlePrompt, null, 2))
+  return "Script Finished!"
+}
+
+const execute = Do().then(console.log)
+
